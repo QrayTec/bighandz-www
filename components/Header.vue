4f357389a3674b79e969fa0c94a5d100fe6e00db
shouzia <saopipizhu@163.com>
2023-04-10 23:35:34 +0800

    🎉 完成NavBar开屏页布局

---
+++ components/Header.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="Header">
+    <div class="HeaderMain">
+      <div class="header_logo">
+        <NuxtLink to="/">
+          <img src="~/assets/index_image/BigHandzCo.Ltd.png" alt="">
+        </NuxtLink>
+      </div>
+      <client-only >
+        <el-menu :default-active="activeIndex" class="el-menu-demo display" mode="horizontal" :ellipsis="false"
+          @select="handleSelect" background-color="#0E1B47" text-color="#FFFFFF" active-text-color="#ffffff" >
+          <div class="flex-grow" />
+            <el-menu-item index="0">ホーム</el-menu-item>
+            <el-menu-item index="1">企業情報</el-menu-item>
+            <el-menu-item index="2">事業概要</el-menu-item>
+            <el-menu-item index="3">採用情報</el-menu-item>
+            <el-menu-item index="4">関連会社</el-menu-item>
+        </el-menu>
+      </client-only>
+      <div class="PhoneNavBar">
+          <div>X</div>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script>
+import { ref } from 'vue'
+
+export default {
+  data() {
+    return {
+      activeIndex: ref('0'),
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+  
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.PhoneNavBar{
+  display: none;
+}
+.Header {
+  width: 100%;
+  height: 80px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #0E1B47;
+  z-index: 100;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header_logo{
+  margin-left: 80px;
+}
+.HeaderMain{
+  width: 100%;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+}
+.flex-grow {
+  flex-grow: 1;
+}
+
+.el-menu-demo {
+  width: 100%;
+  height: 80px;
+  margin-right: 80px;
+}
+
+.header_logo img {
+  width: 68px;
+  height: 48px;
+  margin: 16px 0px;
+}
+
+.el-menu-item {
+  width: 70px;
+  margin: 0px 20px;
+}
+
+.el-menu--horizontal>.el-menu-item.is-active {
+  border-bottom: 4px solid #597BE5;
+  margin: 0px 20px;
+}
+
+.el-menu--horizontal>.el-menu-item {
+  margin: 0px 20px;
+
+}
+
+.el-menu--horizontal {
+  border: none;
+}
+@media only screen and (max-width: 600px){
+  .display{
+    display: none;
+  }
+  .PhoneNavBar{
+    display: block;
+    color: white;
+  }
+  .header_logo{
+    margin: 0;
+  }
+  .Header{
+    height: 60px;
+  }
+  .HeaderMain{
+    width: 100%;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+  }
+}
+</style>
+  
---
+++ pages/index/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="Index">
+    <header>
+      <Header></Header>
+    </header>
+    <main>
+      <section>
+        <div class="First_data">
+          <div class="bg">
+            <div class="First_data_main">
+              <div class="First_Main_title">
+                <div class="Mian_title">
+                  <img src="~/assets/index_image/mian_title.png" alt="">
+                </div>
+                <div class="Mian_title_text">常に先へ</div>
+                <div class="First_data_Content">
+                  顧客様より先に考えること | 顧客様より先に行動すること
+                </div>
+                <div class="Mian_scrool">
+                  <div class="Mian_scrool_content">SCROLL</div>
+                  <div class="Mian_scrool_line"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.First_data {
+  width: 100%;
+  height: 920px;
+  overflow: hidden;
+
+}
+
+.bg {
+  width: 100%;
+  height: 920px;
+  z-index: -1;
+  background-image: url('~/assets/index_image/pic_banner.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.First_data_main{
+  z-index: 1;
+  padding: 80px 80px 0px;
+}
+.Mian_title{
+  margin: 207px 0 0 225px;
+}
+.Mian_title_text{
+  height: 46px;
+  font-size: 48px;
+  font-weight: bold;
+  color: #FFFFFF;
+  margin-top: 86px;
+  margin-left: 278px;
+}
+.First_data_Content{
+  width: 667px;
+  background: #597BE5;
+  font-size: 24px;
+  font-weight: 400;
+  color: #FFFFFF;
+  margin-top: 60px;
+  margin-left: 280px;
+  padding: 9px 23px 9px 19px;
+
+}
+.Mian_scrool{
+
+}
+.Mian_scrool_content{
+  width: 11px;
+  height: 52px;
+  font-size: 14px;
+  font-family: Roboto;
+  font-weight: 400;
+  color: #FFFFFF;
+  line-height: 30px;
+  transform: rotate(270deg);
+}
+.Mian_scrool_line{
+  width: 1px;
+  height: 80px;
+  background: linear-gradient(0deg, rgba(255,255,255,0) 0%, #FFFFFF 100%);
+  transform: translateX(-5px);
+}
+</style>
